7883a33f001e437d3c2fb9e73ec68018986c2da9
hamzaja <hamzajaved71@gmail.com>
2019-10-21 14:38:33 -0400

    JS function that counts the number of characters in a string and returns an object with total numbers of each character from 0-9 and a-z

---
+++ 1-charCount.js
@@ -0,0 +1,45 @@
+// count the number of each chararcter in a string
+const { performance } = require('perf_hooks')
+
+function charCount(string){
+  const result = {}
+  for (let i=0;  i< string.length;i++){
+    let str = string[i].toLowerCase()
+    if (/[a-z0-9]/.test(str)){
+      if (result[str] > 0){
+        result[str] ++
+      }
+      else ( result[str] = 1)
+    }
+  }
+  return result
+}
+
+
+
+
+function charCountUsingASCII(string){
+  const result = {}
+  for (let i=0;  i< string.length;i++
+  ){
+    let str = string[i].toLowerCase()
+    if (str.charCodeAt()> 96 && str.charCodeAt() <123 || str.charCodeAt()> 47 && str.charCodeAt() <58){
+      if (result[str] > 0){
+        result[str] ++
+      }
+      else ( result[str] = 1)
+    }
+  }
+  return result
+}
+let t0 = performance.now()
+charCountUsingASCII("ah123jhsdhbsdg234vj23hg4324h93yr8ufsdif-0o]']']'3]1'3]'31']']'1]1'1]1]]]]]]]]]]]]]]]]]]]''''''''''''''''...................lpplplplplp1lp1lp1lplp3l2p3l2j'324p234[/p32'4l/3l'3[/44;.3'd/flp[rlf,dlmfkfnlksnfjldbfsdl.d][s=f-=-df;d[flld-fp'/sxf/d's[f.;'sdlpcfdsd/fcdsp[flds'cf[sdl1257AGFCDmza")
+let t1 = performance.now()
+let time = t1-t0
+console.log("charCountUsingASCII performance time = " + ( time ) + "MS" )
+
+t0 = performance.now()
+charCount("ah123jhsdhbsdg234vj23hg4324h93yr8ufsdif-0o]']']'3]1'3]'31']']'1]1'1]1]]]]]]]]]]]]]]]]]]]''''''''''''''''...................lpplplplplp1lp1lp1lplp3l2p3l2j'324p234[/p32'4l/3l'3[/44;.3'd/flp[rlf,dlmfkfnlksnfjldbfsdl.d][s=f-=-df;d[flld-fp'/sxf/d's[f.;'sdlpcfdsd/fcdsp[flds'cf[sdl1257AGFCDmza")
+t1 = performance.now()
+time = t1-t0
+console.log("charCount performance time = " + ( time ) + "MS" )
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # js-some-fun-problems
+Code Optimization
+Different ways to solve a problem depending on the time it takes to execute a code
+
+1. charCount : JS function that counts the number of characters in a string and returns an object with total numbers of each character from 0-9 and a-z . I have compared the two Different methods to do this problem. One is where I check every character using /[a-z0-9]/ test and the other one is Checking each character using their ASCII value.
+At the end , it shows that Using ASCII code is faster using performance.now()
