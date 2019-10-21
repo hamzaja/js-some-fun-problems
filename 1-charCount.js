f6d641db7fbd36cc9e525cc0d53ef723807dc354
hamzaja <hamzajaved71@gmail.com>
2019-10-21 14:47:46 -0400

    JS function that counts the number of characters in a string and returns an object with total numbers of each character from 0-9 and a-z

--- 1-charCount.js
+++ 1-charCount.js
@@ -36,10 +36,10 @@ let t0 = performance.now()
 charCountUsingASCII("ah123jhsdhbsdg234vj23hg4324h93yr8ufsdif-0o]']']'3]1'3]'31']']'1]1'1]1]]]]]]]]]]]]]]]]]]]''''''''''''''''...................lpplplplplp1lp1lp1lplp3l2p3l2j'324p234[/p32'4l/3l'3[/44;.3'd/flp[rlf,dlmfkfnlksnfjldbfsdl.d][s=f-=-df;d[flld-fp'/sxf/d's[f.;'sdlpcfdsd/fcdsp[flds'cf[sdl1257AGFCDmza")
 let t1 = performance.now()
 let time = t1-t0
-console.log("charCountUsingASCII performance time = " + ( time ) + "MS" )
+console.log("charCount by Using ASCII value: performance time = " + ( time ) + "MS" )
 
 t0 = performance.now()
 charCount("ah123jhsdhbsdg234vj23hg4324h93yr8ufsdif-0o]']']'3]1'3]'31']']'1]1'1]1]]]]]]]]]]]]]]]]]]]''''''''''''''''...................lpplplplplp1lp1lp1lplp3l2p3l2j'324p234[/p32'4l/3l'3[/44;.3'd/flp[rlf,dlmfkfnlksnfjldbfsdl.d][s=f-=-df;d[flld-fp'/sxf/d's[f.;'sdlpcfdsd/fcdsp[flds'cf[sdl1257AGFCDmza")
 t1 = performance.now()
 time = t1-t0
-console.log("charCount performance time = " + ( time ) + "MS" )
+console.log("charCount without Using ASCII value: performance time = " + ( time ) + "MS" )
