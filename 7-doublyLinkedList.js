29c2264805fa29cbf1d247fb66a6654c5c88c2da
hamzaja <hamzajaved71@gmail.com>
2019-11-06 19:59:08 -0500

    pop method doubly linked list

--- 7-doublyLinkedList.js
+++ 7-doublyLinkedList.js
@@ -10,6 +10,43 @@ class DoublyLinkedList{
   constructor(){
     this.head = null
     this.tail = null
-    this.length = 0 
+    this.length = 0
   }
+  push(val){
+    let node = new Node(val)
+    if(!this.head){
+      this.head = node
+      this.tail = node
+      this.head.next = this.tail
+      this.tail.previous = this.head.next
+    }
+    else{
+      this.tail.next= node
+      node.previous = this.tail
+      this.tail = node
+    }
+    this.length++
+    return this
+  }
+  pop(){
+    if(!this.head) return undefined
+    let tail = this.tail
+    if(this.length===1){
+      this.head = null
+      this.tail = null
+    } else {
+      this.tail = tail.previous
+      this.tail.next = null
+      tail.previous = null
+    }
+    this.length--
+    return tail
+  }
+
 }
+
+let list = new DoublyLinkedList()
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
