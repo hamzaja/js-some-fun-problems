8027d78bff3b153bd4fc497bc8708e104ca0d691
hamzaja <hamzajaved71@gmail.com>
2019-11-11 13:48:34 -0500

    Binary Search tree

---
+++ 10-BinarySearchTree.js
@@ -0,0 +1,13 @@
+class BinarySearchTree{
+  constructor(){
+    this.root = null
+  }
+}
+
+class Node{
+  constructor(val){
+    this.value = val
+    this.right = null
+    this.left = null
+  }
+}
